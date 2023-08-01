1aa605c2cd3ff91e9e57ba6e29c2be15e5571b6d
Ítalo Coelho Araújo <54905334+ItaloCoelhoAraujo@users.noreply.github.com>
2023-08-01 15:00:04 -0300

    Update commands.js
    adicionado timeout aos gets

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,43 +12,43 @@
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 Cypress.Commands.add("busca", (produto) => {
-	cy.get("#twotabsearchtextbox")
+	cy.get("#twotabsearchtextbox", { timeout: 10000 })
 		.should("be.visible")
 		.clear()
 		.type(produto + "{enter}")
 })
 
 Cypress.Commands.add("checarResultadoBusca", (textoParaChecar) => {
-	return cy.contains(textoParaChecar)
+	return cy.contains(textoParaChecar, { timeout: 10000 })
 })
 
 Cypress.Commands.add("selecionarProduto", (numeroNaLista) => {
-	cy.get('[data-component-type="s-search-results"] [data-index="' + (numeroNaLista + 2) + '"] a')
+	cy.get('[data-component-type="s-search-results"] [data-index="' + (numeroNaLista + 2) + '"] a', { timeout: 10000 })
 		.first()
 		.should("be.visible")
 		.click()
-	cy.get("#productTitle").should("exist")
+	cy.get("#productTitle", { timeout: 10000 }).should("exist")
 })
 
 Cypress.Commands.add("pegarTituloProduto", () => {
-	return cy.get("#productTitle").should("be.visible").invoke("text")
+	return cy.get("#productTitle", { timeout: 10000 }).should("be.visible").invoke("text")
 })
 
 Cypress.Commands.add("adicionarAoCarrinho", () => {
-	cy.get("#add-to-cart-button").should("be.visible").click()
+	cy.get("#add-to-cart-button", { timeout: 10000 }).should("be.visible").click()
 })
 
 Cypress.Commands.add("acessarCarrinho", () => {
-	cy.get("#nav-cart-count").should("be.visible").click()
+	cy.get("#nav-cart-count", { timeout: 10000 }).should("be.visible").click()
 })
 
 Cypress.Commands.add("removerDoCarrinho", (tituloProduto) => {
-	cy.contains(tituloProduto).should("exist").as("elementoTitulo", { type: "static" })
-	cy.get(".sc-action-delete > .a-declarative > .a-color-link").should("be.visible").click()
+	cy.contains(tituloProduto, { timeout: 10000 }).should("exist").as("elementoTitulo", { type: "static" })
+	cy.get(".sc-action-delete > .a-declarative > .a-color-link", { timeout: 10000 }).should("be.visible").click()
 })
 
 Cypress.Commands.add("checarSeTemProduto", () => {
-	cy.get("@elementoTitulo").should("not.exist")
+	cy.get("@elementoTitulo", { timeout: 10000 }).should("not.exist")
 })
 //
 //
